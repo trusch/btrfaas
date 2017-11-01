d3c9780b7cd3fd3e34ac17cd243d1d1697287559
Tino Rusch <tino.rusch@gmail.com>
2017-11-01 12:53:28 +0100

    integrated grpc server into example command;

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/trusch/frunner/config"
+	"github.com/trusch/frunner/grpc"
 	"github.com/trusch/frunner/http"
 	"github.com/trusch/frunner/runnable/example"
 )
@@ -16,6 +17,15 @@ func main() {
 
 	cmd := &example.Runnable{}
 
-	server := http.NewServer(cmd, cfg)
-	log.Fatal(server.ListenAndServe())
+	go func() {
+		httpServer := http.NewServer(cmd, cfg)
+		log.Fatal(httpServer.ListenAndServe())
+	}()
+
+	go func() {
+		grpcServer := grpc.NewServer(cmd, cfg)
+		log.Fatal(grpcServer.ListenAndServe())
+	}()
+
+	select {}
 }
