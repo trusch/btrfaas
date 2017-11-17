59438eb4e921de2bc6ce83823e94d76aeb9e2950
Tino Rusch <tino.rusch@gmail.com>
2017-11-17 10:08:05 +0100

    updated examples with new frunner.proto files;

--- examples/btrfaas/native-functions/echo-python/frunner_pb2_grpc.py
+++ examples/btrfaas/native-functions/echo-python/frunner_pb2_grpc.py
@@ -8,11 +8,11 @@ class FunctionRunnerStub(object):
   """FunctionRunner is a service capable of running a function via the Run() method
   the options for the function(s) are stored in the requests metadata under the key `options`
   The `frunner` implementation expects the following metadata structure:
-  metadata = { options: [ "key1=val1", "key2=val2" ] }
+  metadata = { options: [ "val1", "val2" ] }
   The `fgateway` implementation expects this metadata structure:
   metadata = {
   chain: [ "function1", "function2"],
-  options: [ "json-string-string-map-1", "json-string-string-map-2"]
+  options: [ "json-string-array-1", "json-string-array-2"]
   }
   """
 
@@ -33,11 +33,11 @@ class FunctionRunnerServicer(object):
   """FunctionRunner is a service capable of running a function via the Run() method
   the options for the function(s) are stored in the requests metadata under the key `options`
   The `frunner` implementation expects the following metadata structure:
-  metadata = { options: [ "key1=val1", "key2=val2" ] }
+  metadata = { options: [ "val1", "val2" ] }
   The `fgateway` implementation expects this metadata structure:
   metadata = {
   chain: [ "function1", "function2"],
-  options: [ "json-string-string-map-1", "json-string-string-map-2"]
+  options: [ "json-string-array-1", "json-string-array-2"]
   }
   """
 
