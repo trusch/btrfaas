519f9af59ad626162a6c038991500dbbecd98045
Tino Rusch <tino.rusch@gmail.com>
2017-11-17 10:16:28 +0100

    added release script + updated gitignore to exclude build artifacts;

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+echo "running release script"
+echo "first do a clean followed by integration tests"
+# make clean
+# make all
+
+echo "Current Version: " $(git describe)
+read -e -p "New Version Tag:  " TAG
+echo "Tagging current HEAD as ${TAG}"
+git tag -a -m "version ${TAG}" ${TAG}
+
+echo "doing docker pushes"
+docker tag btrfaas/fgateway:latest btrfaas/fgateway:${TAG}
+docker tag btrfaas/frunner:latest btrfaas/frunner:${TAG}
+docker tag btrfaas/fui:latest btrfaas/fui:${TAG}
+docker push btrfaas/fgateway:latest
+docker push btrfaas/frunner:latest
+docker push btrfaas/fui:latest
+docker push btrfaas/fgateway:${TAG}
+docker push btrfaas/frunner:${TAG}
+docker push btrfaas/fui:${TAG}
+
+rm -rf release || true
+mkdir release
+cp btrfaasctl/btrfaasctl release/btrfaasctl
+cp fgateway/fgateway release/fgateway
+cp frunner/cmd/frunner/frunner release/frunner
+cp fui/fui release/fui
+tar cfvj btrfaas-${TAG}.tar.bz2 -C release .
+
+exit 0
