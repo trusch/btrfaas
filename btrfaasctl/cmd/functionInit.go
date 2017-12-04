f2f657b036a2d2687f7e8b1ea7e4761ae8f29b4c
Tino Rusch <tino.rusch@gmail.com>
2017-12-04 08:51:51 +0100

    fixed default reference in function init command;

--- btrfaasctl/cmd/functionInit.go
+++ btrfaasctl/cmd/functionInit.go
@@ -49,7 +49,7 @@ var functionInitCmd = &cobra.Command{
 func init() {
 	functionCmd.AddCommand(functionInitCmd)
 	functionInitCmd.Flags().String("template", "go", "function template to use")
-	functionInitCmd.Flags().String("ref", "v0.3.2", "repo reference to fetch")
+	functionInitCmd.Flags().String("ref", "refs/tags/v0.3.2", "repo reference to fetch")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
